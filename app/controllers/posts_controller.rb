8a1ef54dfd2ee50f12b8cb016cfdae6357fe318f
Naren Kukreja <narenkukreja@gmail.com>
2016-02-14 18:51:18 -0500

    Added helper functions and partials along with flash messages for controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -9,8 +11,35 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to post_path
+    if (@post = Post.create(post_params))
+      flash[:success] = "Post created."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Post could not be created, please check the form again."
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Post could not be updated. Please check the form."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "Post deleted."
+    redirect_to posts_path
   end
 
 end
@@ -19,7 +48,9 @@ private
 
 #Accept image and caption in the create method
 def post_params
-
   params.require(:post).permit(:image, :caption)
-
 end
+
+def find_post
+  @post = Post.find(params[:id])
+end  

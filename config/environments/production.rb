94814a330cf10b7f491b2fc56d39d5a075d78918
Naren Kukreja <narenkukreja@gmail.com>
2016-02-15 15:21:06 -0500

    reorder application.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
-  config.serve_static_assets = true
+
   config.assets.compile = false
   config.assets.compress = false
 
